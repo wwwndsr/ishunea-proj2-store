56c4e7453c8577b276c977a65b2ef9fa8cdfcca9
wwwndsr <cristina.perepeliuc@isa.utm.md>
2025-09-26 16:04:05 +0300

    finished add to cart functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 
 const font = Urbanist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
         className={`${font.className} antialiased`}
       >
         <ModalProvider />
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -9,66 +9,68 @@ import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
 import { useRouter } from "next/navigation";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
-
-interface ProductCard {
-    data: Product;
+interface ProductCardProps {
+  data: Product;
 }
 
-const ProductCard: React.FC<ProductCard> = ({
-    data
-}) => {
+const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  const cart = useCart();
   const previewModal = usePreviewModal();
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleClick = () => {
-      router.push(`/product/${data?.id}`)
-    }
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  };
 
-    const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
-      event.stopPropagation();
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+    previewModal.onOpen(data);  
+  };
 
-      previewModal.onOpen(data);
-    }
+    const onAddToCart: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.stopPropagation();
+    cart.addItem(data); 
+  };
 
-    return ( 
-        <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
-            {/* image sand actions */}
-            <div className="aspect-square rounded-xl bg-gray-100 relative">
-              <Image
-                src={data?.images?.[0]?.url}
-                fill
-                alt="Image"
-                className="aspect-square object-cover rounded-md"
-              />
-              <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
-                <div className="flex gap-x-6 justify-center">
-                    <IconButton 
-                      onClick={onPreview}
-                      icon={<Expand size={20} className="text-gray-600" />}
-                    />
-                    <IconButton 
-                      onClick={() => {}}
-                      icon={<ShoppingCart size={20} className="text-gray-600" />}
-                    />
-                </div>
-              </div>
-            </div>
-            {/* description */}
-            <div>
-              <p className="font-semibold text-lg">
-                {data.name}
-              </p>
-              <p className="text-sm text-gray-500">
-                {data.category?.name}
-              </p>
-            </div>
-            {/* price */}
-            <div className="flex items-center justify-between">
-                <Currency value={data?.price} />
-            </div>
+  return (
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
+      {/* Image and actions */}
+      <div className="aspect-square rounded-xl bg-gray-100 relative">
+        <Image
+          src={data?.images?.[0]?.url || ""}
+          fill
+          alt={data.name}
+          className="aspect-square object-cover rounded-md"
+        />
+        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
+          <div className="flex gap-x-6 justify-center">
+            <IconButton
+              onClick={onPreview}
+              icon={<Expand size={20} className="text-gray-600" />}
+            />
+            <IconButton
+              onClick={onAddToCart}
+              icon={<ShoppingCart size={20} className="text-gray-600" />}
+            />
+          </div>
         </div>
-     );
-}
- 
+      </div>
+      {/* Description */}
+      <div>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category?.name}</p>
+      </div>
+      {/* Price */}
+      <div className="flex items-center justify-between">
+        <Currency value={data?.price} />
+      </div>
+    </div>
+  );
+};
+
 export default ProductCard;
